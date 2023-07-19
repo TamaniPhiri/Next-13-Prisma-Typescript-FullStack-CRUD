fa6a773df9595510fce48bd0011e36c92f8efd76
Tamani Phiri <tamanigabriel0@gmail.com>
2023-07-19 18:14:49 +0200

    success push to array

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,50 +12,50 @@ interface FormData {
   id: string;
 }
 
-interface Notes{
-  notes:{
-    id: string,
-    title: string,
-    description: string
-  }[]
+interface Notes {
+  notes: {
+    id: string;
+    title: string;
+    description: string;
+  }[];
 }
 
-export default function Home({notes}:Notes) {
-  const router=useRouter()
+export default function Home({ notes }: Notes) {
+  const router = useRouter();
   const [form, setForm] = useState<FormData>({
     title: "",
     description: "",
     id: "",
   });
 
-  const refreshData =()=>{
-    router.replace(router.asPath)
-  }
+  const refreshData = () => {
+    router.replace(router.asPath);
+  };
 
-  async function create(data: FormData){
+  async function create(data: FormData) {
     try {
-      fetch('http://localhost:3000/api/create',{
+      fetch("http://localhost:3000/api/create", {
         body: JSON.stringify(data),
-        headers:{
-          'Content-Type': 'application/json'
+        headers: {
+          "Content-Type": "application/json",
         },
-        method:'POST'
-      }).then(()=>{
-        setForm({title:"", description:"",id:""})
-        refreshData()
-      })
+        method: "POST",
+      }).then(() => {
+        setForm({ title: "", description: "", id: "" });
+        refreshData();
+      });
     } catch (error) {
       console.log(error);
     }
   }
 
-  const handleSubmit=async(data:FormData)=>{
+  const handleSubmit = async (data: FormData) => {
     try {
-      create(data)
+      create(data);
     } catch (error) {
       console.log(error);
     }
-  }
+  };
   return (
     <main
       className={`${inter.className} flex w-full gap-3 flex-col px-4 md:px-10 justify-center items-center min-h-screen`}
@@ -81,39 +81,38 @@ export default function Home({notes}:Notes) {
           value={form.description}
           onChange={(e) => setForm({ ...form, description: e.target.value })}
         />
-        <button type="submit" className="w-72 bg-blue-800 hover:bg-blue-900 flex items-center justify-center p-2 rounded">
+        <button
+          type="submit"
+          className="w-72 bg-blue-800 hover:bg-blue-900 flex items-center justify-center p-2 rounded"
+        >
           Save
         </button>
       </form>
       <div className="flex flex-col w-72 pt-5 gap-4 items-center">
-        {notes.map(note =>{
-          return(
+        {notes.map((note) => {
+          return (
             <div key={note.id} className="flex w-full border-b flex-col gap-2">
-              <span className="font-bold">
-              {note.title}
-              </span>
-              <p className="font-bold text-gray-400">
-                {note.description}
-              </p>
+              <span className="font-bold">{note.title}</span>
+              <p className="font-bold text-gray-400">{note.description}</p>
             </div>
-          )
+          );
         })}
       </div>
     </main>
   );
 }
 
-export const getServerSideProps:GetServerSideProps=async()=>{
-  const notes=await prisma?.notes.findMany({
-    select:{
-      title:true,
-      id:true,
-      description:true
-    }
-  })
-  return{
-    props:{
-      notes
-    }
-  }
-}
+export const getServerSideProps: GetServerSideProps = async () => {
+  const notes = await prisma?.notes.findMany({
+    select: {
+      title: true,
+      id: true,
+      description: true,
+    },
+  });
+  return {
+    props: {
+      notes,
+    },
+  };
+};
